e32218565040d0ec04733ebe3e4858f46cd4ae8d
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-17 13:31:07 -0500

    Create merge_sorted_lists.py

---
+++ txts/eopi/merge_sorted_lists.py
@@ -0,0 +1,32 @@
+	
+	
+class Node:
+	"""Node class for Linked Lists"""
+	
+	def __init__(self, data=None, next=None):
+		self.data = data
+		self.next = next
+		
+	def __str__(self):
+		return str(self.data)
+	
+	
+def merge_two_sorted_lists(list1, list2):
+	"""Page 113"""
+	dummy_head = Node()
+	current = dummy_head
+	p1 = list1
+	p2 = list2
+	
+	while p1 and p2:
+		if p1.data <= p2.data:
+			current.next = p1
+			p1 = p1.next
+		else:
+			current.next = p2
+			p2 = p2.next
+		current = current.next
+
+	# Appends the remaining nodes of p1 or p2
+	current.next = p1 if p1 else p2
+	return dummy_head.next
