c6b99fb9babe84571d99e0786013eebc960fc160
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-17 16:18:38 -0500

    Create two-sum.py

---
+++ two-sum.py
@@ -0,0 +1,14 @@
+# Beats 96.40% of Python submissions as of 11/17/2016
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pairs = {}
+        for i, n in enumerate(nums):
+            if not target - n in pairs:
+                pairs[n] = i
+            else:
+                return [pairs[target - n], i]
