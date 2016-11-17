255a71ad4c59646f218a0d434dd4554a6b89f229
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-17 13:48:32 -0500

    Update merge_sorted_lists.py

--- txts/eopi/merge_sorted_lists.py
+++ txts/eopi/merge_sorted_lists.py
@@ -6,6 +6,9 @@ class Node:
 	def __init__(self, data=None, next=None):
 		self.data = data
 		self.next = next
+		
+	def __str__(self):
+		return str(self.data)
 	
 	
 def merge_two_sorted_lists(list1, list2):
