b0c1039cfbcc67e23a655cd339c9987e6a20b3cc
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-18 13:34:44 -0500

    Create number_of_islands.py

---
+++ leetcode/number_of_islands.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/number-of-islands/
+
+import collections
+
+class Solution(object):
+    
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        island_count = 0
+        for i, row in enumerate(grid):
+            for j, char in enumerate(row):
+                if char == '1':  # Island
+                    island_count += 1
+                    self.bfs(grid, i, j)
+        return island_count
+        
+    def bfs(self, grid, i, j):
+        q = collections.deque()
+        grid[i][j] = '-1'
+        q.append((i, j))
+        while q:
+            # Indices to check
+            i, j = q.popleft()
+            
+             # Add above
+            if (i-1) >= 0 and grid[i-1][j] == '1':
+                q.append((i-1,j))
+                grid[i-1][j] = '-1'
+                
+            # Add below
+            if (i+1) < len(grid) and grid[i+1][j] == '1':   
+                q.append((i+1,j))
+                grid[i+1][j] = '-1'
+                  
+            # Add left  
+            if (j-1) >= 0 and grid[i][j-1] == '1':  
+                q.append((i,j-1))
+                grid[i][j-1] = '-1'
+                
+            # Add right
+            if (j+1) < len(grid[i]) and grid[i][j+1] == '1':
+                q.append((i,j+1))
+                grid[i][j+1] = '-1'
+            
+        
