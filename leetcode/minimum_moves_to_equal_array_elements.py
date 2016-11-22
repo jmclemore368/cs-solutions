796503d436c74b9f4487d4aeab9b132a28af0578
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-21 22:13:09 -0500

    Update minimum_moves_to_equal_array_elements.py

--- leetcode/minimum_moves_to_equal_array_elements.py
+++ leetcode/minimum_moves_to_equal_array_elements.py
@@ -10,7 +10,7 @@ class Solution(object):
 
         smallest_element = min(nums)
 
-        # The number of moves is the sum of all Xi - smallest_element
+        # The number of moves is the sum of all (Xi - smallest_element)
         # I.e., given [1, 2, 3], the smallest element is 1.
         # The minimum number of moves is (1-1) + (2-1) + (3-1) = 3
         min_num_moves = sum(num - smallest_element for num in nums)
