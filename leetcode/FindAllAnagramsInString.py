f944ea798ad93c1c300f94df02cc4c16fa561206
Jeffrey McLemore <jmclemore368@gmail.com>
2016-11-29 11:24:04 -0500

    Update FindAllAnagramsInString.py

--- leetcode/FindAllAnagramsInString.py
+++ leetcode/FindAllAnagramsInString.py
@@ -16,21 +16,18 @@ class Solution(object):
         # Maintain a window of len(p) in s, and slide to right until finish. 
         for i in range(len(p) - 1, len(s)):
             
-            # Include a new char
+            # Include a new char in the sliding window
             s_counter[s[i]] += 1
             
-            # This step is O(1), since there are at most 26 letters in the alphabet
+            # The comparison is O(1), since there are at most 26 letters in the alphabet
             if s_counter == p_counter:
                 result.append(i - len(p) + 1)
                 
-            # Remove oldest char in the sliding window as we are sliding right
+            # Remove oldest char in the sliding window. Delete it if count goes to 0.
             s_counter[s[i - len(p) + 1]] -= 1
-            
-            # Remove the oldest char count if it is 0
             if not s_counter[s[i - len(p) + 1]]:
                 del s_counter[s[i - len(p) + 1]]
-        
-        
+
         return result
     
             
