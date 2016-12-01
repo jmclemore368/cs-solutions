be0ebfdea341bf3d99709e8c1087f8235484fb93
Jeffrey McLemore <jmclemore368@gmail.com>
2016-12-01 14:10:07 -0500

    Update MoveZeroes.py

--- leetcode/MoveZeroes.py
+++ leetcode/MoveZeroes.py
@@ -15,7 +15,7 @@ class Solution(object):
         p2 = 0
         while p2 < len(nums):
             if nums[p2] != 0:
-                nums[p1], nums[p2] = nums[p2], nums[p1]
+                nums[p1], nums[p2] = nums[p2], nums[p1]  # swap
                 p1 += 1
             p2 += 1
     
