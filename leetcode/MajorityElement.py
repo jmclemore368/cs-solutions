e081a4122b7035e6cb52d4f4b4393bc5a7666017
Jeffrey McLemore <jmclemore368@gmail.com>
2016-12-01 13:41:45 -0500

    Update MajorityElement.py

--- leetcode/MajorityElement.py
+++ leetcode/MajorityElement.py
@@ -19,6 +19,6 @@ class Solution(object):
             else:
                 count -= 1
                 
-         # Problem guarantees majority element. 
+        # Problem guarantees majority element. 
         # Otherwise: before returning check if count is > 0. If not, there is no majority.
         return major  
