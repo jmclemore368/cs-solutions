10807b5e9ed046060d3215fdfee0745c04bb15bc
Jeffrey McLemore <jmclemore368@gmail.com>
2016-12-01 01:46:27 -0500

    Update PathSum3.py

--- leetcode/PathSum3.py
+++ leetcode/PathSum3.py
@@ -16,10 +16,34 @@ class Solution(object):
         :rtype: int
         """
         prefix_sums = {}
-        prefix_sums[0] = 1
+        prefix_sums[0] = 1 # Edge case: for when the tree contains only 1 node.
         return self.pathSumUtil(root, 0, sum, prefix_sums)
         
     def pathSumUtil(self, curr, sum, tar, prefix_sums):
+        """
+        A prefix sum for a node is the sum of values from the root to that node.
+        Consider some node C, and any other arbitrary node N that is on the path between the root and C.
+        Then the (sum from C -> N) = (prefix sum of C) - (prefix sum of N)
+        
+        Rearranging, note the (prefix sum of N) = (prefix sum of C) - (sum from C -> N)
+        Since we are searching for a specific target sum, then (sum from C -> N) = tar
+        Thus, we see that (prefix sum of N) = (prefix sum of C) - tar
+        
+        The strategy is to keep track of all the prefix sums in a hash table. 
+        Note that there may be multiple prefix sums that are equal, so we also keep track of frequencies.
+        Noting the above, for every prefix sum P in the recursion, we check to see if P - tar exists in our table.
+        If P - tar exists in the table, it implies there is some path(s) that sum to target.
+        
+        As an edge case, note that the recursion is calculated bottom-up.
+        Thus we must be sure to remove (or decrement) the prefix sum P when we return since it is no longer in consideration. 
+              10
+             /  \
+            5   -3
+           / \    \
+          3   2   11
+         / \   \
+        3  -2   1
+        """
         if curr is None:
             return 0
         
