6179239d4274fe8799f116558e9d7a1985a52dfc
Jeffrey McLemore <jmclemore368@gmail.com>
2016-12-02 22:07:50 -0500

    Update RankOfWord.py

--- HackerRank/RankOfWord.py
+++ HackerRank/RankOfWord.py
@@ -2,53 +2,45 @@
 import math
 import collections
 
-def find_num_words(word):
+def find_num_permutations(word):
+    """
+    The number of permutations of a string of length n is: n! / (c1! * c2 * ... * cn!)
+    Where ck gives the number of occurrences of each distinct character.
+    
+    For example, given "SUCCSS", the number of permutations is then:
+        6!
+     -------
+     3! * 2!
+    
+    """
     c = collections.Counter(word)
-
-    # Get all the counts of duplicates
-    duplicates = [c[key] for key in c.iterkeys() if c[key] > 1]
-
-    denominator = 1
-    for d in duplicates:
-        denominator *= math.factorial(d)
-
-    print duplicates
-    print "num: " + str(len(word)) + "!"
-    print "denom: " + str(denominator)
-
-    return math.factorial(len(word)) / denominator
+    numerator = math.factorial(len(word))
+    denominator = 1 
+    for key in c.iterkeys():
+        denominator *= math.factorial(c[key])
+    return numerator / denominator
 
 def find_rank_util_possible_repetition(word, sorted_word, rank, index):
-
-    #print "\nWord is: " + word
-    #print "Sorted word is: " + sorted_word
-    #print "Char to find is: " + word[index]
-
     if index == len(word) - 1:
-        #print 'final answer is: ' + str(rank)
         return rank + 1
 
     i = 0
     while i < len(sorted_word):
         remaining_chars = sorted_word[:i] + sorted_word[i + 1:]
-        #print "Looking at char " + str(i) + " of word " + sorted_word + ": " + sorted_word[i]
-        #print "Is " + sorted_word[i] + " == " + word[index] + "?"
+
         if sorted_word[i] == word[index]:
-            #print "Yes. Fix this character. Recurse on: " + remaining_chars
             return find_rank_util_possible_repetition(word, remaining_chars, rank, index + 1)
         else:
-            num_words = find_num_words(remaining_chars)
-            print "No. How many words can be made from " + remaining_chars + "?: " + str(num_words)
-            rank += num_words
-
+            rank += find_num_permutations(remaining_chars)
+            # If the next chars are the same, then they will not create any new words. Skip over these.
             while i < len(sorted_word) and sorted_word[i] == sorted_word[i + 1]:
                 i += 1
+                
         i += 1
 
+        
 def find_rank(word):
-
     sorted_word = ''.join(sorted(word))
-    print 'sorted word is: ' + sorted_word
     return find_rank_util_possible_repetition(word, sorted_word, 0, 0)
 
 
@@ -56,7 +48,6 @@ num_cases = int(raw_input())
 result = []
 for i in range(num_cases):
     word = raw_input().strip().lower()
-    #rint 'word is: ' + word
     rank = find_rank(word)
     result.append(rank)
 
