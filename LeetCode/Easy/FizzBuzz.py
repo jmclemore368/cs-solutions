f421f8f73e905a61db9a614e0f0f61304042ee03
Jeffrey McLemore <jmclemore368@gmail.com>
2016-12-09 17:14:44 -0500

    Update FizzBuzz.py

--- LeetCode/Easy/FizzBuzz.py
+++ LeetCode/Easy/FizzBuzz.py
@@ -7,6 +7,31 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        
+        # Initialize values
+        result = [str(i) for i in range(1, n + 1)]
+
+        for i in range(1, n + 1):
+            s = []
+            if i % 3 == 0: 
+                s.extend(list("Fizz"))
+            if i % 5 == 0: 
+                s.extend(list("Buzz"))
+            if s:
+                result[i - 1] = ''.join(s)
+                
+        return result
+
+
+"""
+# Correct, but significantly slower due to more comparisons. 
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
         result = []
         for i in range(1, n + 1):
             if (i % 15) == 0:
@@ -18,3 +43,5 @@ class Solution(object):
             else:
                 result.append(str(i))
         return result
+"""
+    
